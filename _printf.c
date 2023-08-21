0ef497ea8f900d0e7eb01dcb49ad3382e493abd0
yassine <118019022+ouilh@users.noreply.github.com>
2023-08-21 18:11:24 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h> 
 
 /**
  * theprint - selects the function to use based on the given format specifier.
@@ -10,51 +9,41 @@
  * Return: a pointer to the function corresponding to the format specifier.
  *         NULL if no matching function is found.
  */
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	int i, j;
+	int (*printfunct)(va_list);
+	va_list args;
 
-int _putchar(char c) {
-   
-    return write(1, &c, 1);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+	va_start(args, format);
+
+	for (i = 0; format != NULL && format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			for (j = 0; format[j + 1] == ' '; j++)
+			{
+				if (format[j + 2] == '\0')
+					return (-1);
+			}
+			i++;
+			printfunct = theprint(&format[i]);
+
+			if (printfunct != NULL)
+				count += printfunct(args);
+			else
+			{
+				count += _putchar('%');
+				count += _putchar(format[i]);
+			}
+		}
+		else
+			count += _putchar(format[i]);
+	}
+	va_end(args);
+	return (count);
 }
-
-int printchar(va_list args) {
-    int c = va_arg(args, int);
-    return _putchar(c);
-}
-
-int printstring(va_list args) {
-    char *str = va_arg(args, char*);
-    int count = 0;
-    while (*str != '\0') {
-        count += _putchar(*str);
-        str++;
-    }
-    return count;
-}
-
-int printpercent(va_list args) {
-    return _putchar('%');
-}
-
-int printint(va_list args) {
-    int num = va_arg(args, int);
-    int count = 0;
-   
-    return count;
-}
-
-int printinteger(va_list args) {
-    int num = va_arg(args, int);
-    return printint(args);
-}
-
-int (*theprint(const char *format))(va_list) {
-   
-    return NULL;
-}
-
-int _printf(const char *format, ...) {
-   
-    return 0; 
-}
-
-
