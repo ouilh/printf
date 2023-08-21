fb6fd362513c057d4b37429b8ceaa93bea690bb4
charaforhanou <charaforhanou@gmail.com>
2023-08-21 08:01:55 -0700

    comment

--- _printf.c
+++ _printf.c
@@ -1,60 +1,48 @@
 #include "main.h"
-#include <stdio.h> 
 
 /**
- * theprint - selects the function to use based on the given format specifier.
+ * _printf - Produces output according to a format.
  *
- * @format: the format specifier character.
- * @args: a va_list of arguments for the selected function.
+ * @format: The format string containing format specifiers.
  *
- * Return: a pointer to the function corresponding to the format specifier.
- *         NULL if no matching function is found.
+ * Return: Number of characters printed (excluding the null byte).
  */
-
-int _putchar(char c) {
-   
-    return write(1, &c, 1);
-}
-
-int printchar(va_list args) {
-    int c = va_arg(args, int);
-    return _putchar(c);
-}
-
-int printstring(va_list args) {
-    char *str = va_arg(args, char*);
+int _printf(const char *format, ...)
+{
     int count = 0;
-    while (*str != '\0') {
-        count += _putchar(*str);
-        str++;
+    int i, j;
+    int (*printfunct)(va_list);
+    va_list args;
+
+    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+        return (-1);
+
+    va_start(args, format);
+
+    for (i = 0; format != NULL && format[i] != '\0'; i++)
+    {
+        if (format[i] == '%')
+        {
+            for (j = 0; format[j + 1] == ' '; j++)
+            {
+                if (format[j + 2] == '\0')
+                    return (-1);
+            }
+            i++;
+            printfunct = theprint(&format[i]);
+
+            if (printfunct != NULL)
+                count += printfunct(args);
+            else
+            {
+                count += _putchar('%');
+                count += _putchar(format[i]);
+            }
+        }
+        else
+            count += _putchar(format[i]);
     }
-    return count;
+    va_end(args);
+    return (count);
 }
-
-int printpercent(va_list args) {
-    return _putchar('%');
-}
-
-int printint(va_list args) {
-    int num = va_arg(args, int);
-    int count = 0;
-   
-    return count;
-}
-
-int printinteger(va_list args) {
-    int num = va_arg(args, int);
-    return printint(args);
-}
-
-int (*theprint(const char *format))(va_list) {
-   
-    return NULL;
-}
-
-int _printf(const char *format, ...) {
-   
-    return 0; 
-}
-
 
--- main.h
+++ main.h
@@ -2,12 +2,14 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <unistd.h>
 
+/* Function prototypes */
 int _putchar(char c);
 int printchar(va_list args);
 int printstring(va_list args);
 int printpercent(va_list args);
-int printint(va_list args);
+int printint(int n);
 int printinteger(va_list args);
 int (*theprint(const char *format))(va_list);
 int _printf(const char *format, ...);
