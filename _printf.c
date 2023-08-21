3865475f82950dd6b546a13f33286fb09e5465b3
ouilh <yassine22.com@gmail.com>
2023-08-21 08:23:38 -0700

    add

--- _printf.c
+++ _printf.c
@@ -9,40 +9,41 @@
  */
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    int i, j;
-    int (*printfunct)(va_list);
-    va_list args;
+	int count = 0;
+	int i, j;
+	int (*printfunct)(va_list);
+	va_list args;
 
-    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-        return (-1);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	    
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
 
-    for (i = 0; format != NULL && format[i] != '\0'; i++)
-    {
-        if (format[i] == '%')
-        {
-            for (j = 0; format[j + 1] == ' '; j++)
-            {
-                if (format[j + 2] == '\0')
-                    return (-1);
-            }
-            i++;
-            printfunct = theprint(&format[i]);
+	for (i = 0; format != NULL && format[i] != '\0'; i++)
+	{
+        	if (format[i] == '%')
+        	{
+            		for (j = 0; format[j + 1] == ' '; j++)
+            		{
+                		if (format[j + 2] == '\0')
+                    		return (-1);
+            		}
+            		i++;
+            		printfunct = theprint(&format[i]);
 
-            if (printfunct != NULL)
-                count += printfunct(args);
-            else
-            {
-                count += _putchar('%');
-                count += _putchar(format[i]);
-            }
-        }
-        else
-            count += _putchar(format[i]);
-    }
-    va_end(args);
-    return (count);
+            		if (printfunct != NULL)
+                		count += printfunct(args);
+            		else
+            		{
+                		count += _putchar('%');
+                		count += _putchar(format[i]);
+            		}
+        	}
+        	else
+            		count += _putchar(format[i]);
+	}
+    	va_end(args);
+    	return (count);
 }
 
