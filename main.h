31159b4f48619479c14ebe3862ac6cffda5343c5
yassine <118019022+ouilh@users.noreply.github.com>
2023-08-22 14:23:26 +0100

    Update main.h

--- main.h
+++ main.h
@@ -14,6 +14,11 @@ int printinteger(va_list args);
 int (*theprint(const char *format))(va_list);
 int _printf(const char *format, ...);
 
+/**
+ * format - Represents a formatting specifier and its corresponding function.
+ * @specf: Formatting specifier string.
+ * @function: Pointer to the function that processes data using the specifier.
+ */
 typedef struct formats
 {
 char *specf;
